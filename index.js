fe760bbeb92a82dd182b00462bbf8444b30bdbab
valopezs <valeriabrilopez@gmail.com>
2022-08-23 19:16:43 -0500

    added questions for readme

--- index.js
+++ index.js
@@ -1,7 +1,134 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Welcome to the README generator! What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            }   else {
+                console.log('Please enter the title of your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub Username?',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Link your Github so users can find your work');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Enter an email so you can be contacted');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'decription',
+        message: 'Enter your project description:',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Provide the best description possible for your project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the instructions for installation?',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please provide instructions for installation');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Instructions for usage:',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Provide instructions so users can properly navigate your project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can other developers contribute to this project?',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('Provide instructions on how others can contribute to your project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Describe the tests written for your project and how to use them:',
+        validate: testsInput => {
+            if (testsInput) {
+                return true;
+            } else {
+                console.log('Provide instructions on how others can contribute to your project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmLicenses',
+        message: 'Would you like to include a license?',
+        default: false
+    },
+    {
+        type: 'list',
+        name: 'licenses',
+        message: 'What licenses would you like to include?',
+        choices: ['MIT', 'GPL', 'CC--0'],
+        when: ({ confirmLicenses }) => {
+            if (confirmLicenses) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
